f9d4ffa27d589be7b9aef961d9b1bb7db1cb1f22
Samith Darshana <samithdarshana49@gmail.com>
2024-06-29 09:31:54 +0530

    modified route 5

--- frontend/src/pages/NewBlog.jsx
+++ frontend/src/pages/NewBlog.jsx
@@ -4,50 +4,62 @@ import axios from "axios";
 import PropTypes from "prop-types";
 
 const NewBlog = () => {
+  const [username, setUsername] = useState("");
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
+  const [tags, setTags] = useState("");
+  const [popup, setPopup] = useState({ show: false, message: "", type: "" });
 
-  const [id, setId] = useState("")
-  const [title, setTitle] = useState("")
-  const [content, setContent] = useState("")
-  const [tags, setTags] = useState("") 
-  const [popup, setPopup] = useState({ show: false, message: '', type: '' })
-
-  const handleSubmit = async (e) => { 
-    e.preventDefault()
-    try { 
-      await axios.post(`http://localhost:4000/api/blog/addblog/${id}`, {title, content, tags}) 
-      setPopup({ show: true, message: 'Blog post added successfully!', type: 'success' })
-      setTitle('')
-      setContent('')
-      setTags('')
-      setId('')
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      await axios.post(`http://localhost:4000/api/blog/`, {
+        username,
+        title,
+        content,
+        tags,
+      });
+      setPopup({
+        show: true,
+        message: "Blog post added successfully!",
+        type: "success",
+      });
+      setTitle("");
+      setContent("");
+      setTags("");
+      setUsername("");
     } catch (error) {
-      console.log(error)
-      setPopup({ show: true, message: 'Failed to add blog post.', type: 'error' })
+      console.log(error);
+      setPopup({
+        show: true,
+        message: "Failed to add blog post.",
+        type: "error",
+      });
     } finally {
       setTimeout(() => setPopup({ ...popup, show: false }), 3000); // Hide popup after 3 seconds
     }
-  }  
+  };
 
   return (
     <div className="flex flex-col justify-center items-center p-5 pl-28 pr-28">
       <div className="w-full mx-auto mt-10 p-6 bg-slate-100 rounded-lg shadow-md flex flex-col items-center justify-center">
-        <h1 className="text-teal-600 text-xl font-bold mb-4">Create New Blog Post</h1> 
-        {popup.show && (
-                <Popup message={popup.message} type={popup.type} />
-            )}
+        <h1 className="text-teal-600 text-xl font-bold mb-4">
+          Create New Blog Post
+        </h1>
+        {popup.show && <Popup message={popup.message} type={popup.type} />}
         <form className="w-11/12  ">
-        <div className="mb-4">
+          <div className="mb-4">
             <label
-              htmlFor="userId"
-              className="block text-gray-700 font-bold mb-2" 
+              htmlFor="username"
+              className="block text-gray-700 font-bold mb-2"
             >
-              User Id:{" "}
+              User Name:{" "}
             </label>
             <input
-              type="number"
-              id="userId"
-              value={id}
-              onChange={(e) => setId(e.target.value)}
+              type="text"
+              id="username"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
               className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
             />
           </div>
@@ -118,15 +130,19 @@ const NewBlog = () => {
 
 const Popup = ({ message, type }) => {
   return (
-      <div className={`fixed top-4 right-4 p-4 rounded shadow-md z-50 ${type === 'success' ? 'bg-green-500 text-white' : 'bg-red-500 text-white'}`}>
-          {message}
-      </div>
+    <div
+      className={`fixed top-4 right-4 p-4 rounded shadow-md z-50 ${
+        type === "success" ? "bg-green-500 text-white" : "bg-red-500 text-white"
+      }`}
+    >
+      {message}
+    </div>
   );
 };
 
 Popup.propTypes = {
   message: PropTypes.string.isRequired,
-  type: PropTypes.string.isRequired, 
+  type: PropTypes.string.isRequired,
 };
 
 export default NewBlog;
